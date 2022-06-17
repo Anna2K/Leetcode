512f91f98bea952bbd6f4eb5f8707ad48886b73d
Anna Dorren <107654101+Anna2K@users.noreply.github.com>
2022-06-17 15:06:51 +0200

    Time: 127 ms (5.10%), Space: 14.8 MB (39.92%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -5,15 +5,17 @@ class Solution:
 
         l, r = 0, len(nums) - 1
 
-        while l < r:
+        while l <= r:
             mid = l + (r - l) // 2
 
             if nums[mid] == target:
                 return mid
+            
             elif nums[mid] < target:
                 l = mid + 1
+                
             else:
-                r = mid
+                r = mid - 1
 
-        return l + 1 if nums[l] < target else l
+        return l
 
