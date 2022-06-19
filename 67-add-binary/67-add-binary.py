6cdfc87e82b812890d1d1054c95d4a85f56f3c26
Anna Dorren <107654101+Anna2K@users.noreply.github.com>
2022-06-19 03:24:13 +0200

    Time: 43 ms (69.81%), Space: 13.9 MB (24.23%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,27 +1,3 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        if not a:
-            return b
-        if not b:
-            return a
-        
-        if len(a) < len(b):
-            a, b = b, a
-        a = a[::-1]
-        b = b[::-1]
-        carry = 0
-        res = []
-        
-        for i in range(len(a)):
-            if i < len(b):
-                curr_sum = int(a[i]) + int(b[i]) + carry
-            else:
-                curr_sum = int(a[i]) + carry
-                
-            carry = curr_sum // 2
-            res.append(str(curr_sum % 2))
-        
-        if carry:
-            res.append(str(carry))
-            
-        return ''.join(res)[::-1]
+        return str(bin(int(a, 2) + int(b, 2)))[2:]
